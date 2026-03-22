35ba6b3f20b476bb391f910ddf128c3d243b62fd
Smartlinkx <fiber.smartlink@gmail.com>
2026-03-23 07:56:07 +0800

    config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js - UPDATE TO THIS
 window.APP_CONFIG = {
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxlG3BRNWs-ygE1jxgJs3NUwUa1F0p_zo6E8-x78vuVu_Py85bd-RDtjOvlbcMTWxK0mw/exec"
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbwOzySGTDkK09hHwUact_kPBQw0RHpKLFCT_3jVPA4dK7aK1ZF_X6QjTJpEuczbMihReg/exec"
 };
 
 console.log("Config loaded:", window.APP_CONFIG);
