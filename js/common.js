a6db8bef5de73e0d5352e13118e4b1656cb66891
Smartlinkx <fiber.smartlink@gmail.com>
2026-03-23 13:21:28 +0800

    common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,5 @@
 const AUTH_STORAGE_KEY = "smartlinkx_current_user";
 const API_STORAGE_KEY = "smartlinkx_api_base_url";
-const DEFAULT_API_BASE_URL = "https://script.google.com/macros/s/AKfycbxZuGk556u6dgYQO0f-ec6idosk8qXPnzugdRMUwqjtYd_4tQeEorQWoAxoHog_OeLa6g/exec";
 
 (function ensureAppConfig() {
   if (!window.APP_CONFIG) window.APP_CONFIG = {};
